d4e91dfa19e46121e5756c1d0a7ef61d0819ff62
João Costa <joao.costa@amaiseducaca.com.br>
2022-09-23 11:28:41 +0000

    feat:  handle internal errors return

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -35,4 +35,30 @@ class Controller extends BaseController
 
         return !$allCostCenters->isEmpty();
     }
+
+    /**
+     * Return user friendly error messages
+     *
+     * @param  string $error_message
+     * @return string
+     */
+    public function handleErrorMessage(string $error_message)
+    {
+        $error_message_return = 'Internal error.';
+
+        $foreign_key_error = 'foreign key constraint fails';
+        $foreign_key_pos = strpos($error_message, $foreign_key_error);
+
+        if ($foreign_key_pos) {
+            $table_name_start_pos = strpos($error_message, '`.`', $foreign_key_pos) + 2;
+            $table_name_end_pos = strpos($error_message, ',', $table_name_start_pos);
+            $table_name_length = $table_name_end_pos - $table_name_start_pos;
+
+            $error_message_return .= ' Cannot delete or update: this resource is used by ';
+            $error_message_return .= substr($error_message, $table_name_start_pos, $table_name_length);
+            $error_message_return .= '.';
+        }
+
+        return $error_message_return;
+    }
 }
