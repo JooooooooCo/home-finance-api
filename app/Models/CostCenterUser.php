0be6b66becbe110be71817c19107deec399ccb13
JooooooooCo <costa.joaomf@gmail.com>
2024-05-23 22:10:35 -0300

    chore: add softdelete on models

--- app/Models/CostCenterUser.php
+++ app/Models/CostCenterUser.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CostCenterUser extends Model
 {
+    use SoftDeletes;
     protected $table = 'cost_center_user';
 
     protected $fillable = [
