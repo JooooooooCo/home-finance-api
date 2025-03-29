b1e4af63597983ef9f6a01154806e7f7c0f0ebf4
Joao Costa <costa.joaomf@gmail.com>
2025-03-28 23:03:14 -0300

    refact: remove cost center routes from settings scope

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::post('/user/login', 'App\Http\Controllers\UserAuthController@login');
 Route::middleware('auth:api')->group(function () {
     Route::post('/user/logout', 'App\Http\Controllers\UserAuthController@logout');
     Route::get('/user/details', 'App\Http\Controllers\UserAuthController@details');
+    Route::get('/cost-center', 'App\Http\Controllers\CostCenterController@list');
+    Route::post('/cost-center', 'App\Http\Controllers\CostCenterController@create');
+    Route::get('/cost-center/{id}', 'App\Http\Controllers\CostCenterController@get');
+    Route::put('/cost-center/{id}', 'App\Http\Controllers\CostCenterController@update');
+    Route::delete('/cost-center/{id}', 'App\Http\Controllers\CostCenterController@delete');
 });
 
 require __DIR__ . '/api/settings.php';
--- routes/api/settings.php
+++ routes/api/settings.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/settings/cost-center', 'App\Http\Controllers\CostCenterController@list');
-    Route::post('/settings/cost-center', 'App\Http\Controllers\CostCenterController@create');
-    Route::get('/settings/cost-center/{id}', 'App\Http\Controllers\CostCenterController@get');
-    Route::put('/settings/cost-center/{id}', 'App\Http\Controllers\CostCenterController@update');
-    Route::delete('/settings/cost-center/{id}', 'App\Http\Controllers\CostCenterController@delete');
+
 });
