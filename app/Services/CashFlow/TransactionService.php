15dbb8e5ebc096463210f5d81595d770b31e0135
Joao Costa <costa.joaomf@gmail.com>
2025-04-23 23:13:05 -0300

    feat: add limit to transactions list

--- app/Services/CashFlow/TransactionService.php
+++ app/Services/CashFlow/TransactionService.php
@@ -2,11 +2,13 @@
 
 namespace App\Services\CashFlow;
 
+use Exception;
 use App\Models\CashFlow\Transaction;
 use App\Repositories\CashFlow\Interfacies\TransactionRepositoryInterface;
 
 class TransactionService
 {
+    private const MAX_ALLOWED_LIST_RESPONSE = 1000;
     protected $repository;
 
     public function __construct(TransactionRepositoryInterface $repository)
@@ -16,7 +18,15 @@ class TransactionService
 
     public function list(array $filters): array
     {
-        return $this->repository->getAll($filters);
+        $transactions = $this->repository->getAll($filters);
+
+        if (count($transactions) > self::MAX_ALLOWED_LIST_RESPONSE) {
+            throw new Exception(
+                "O filtro aplicado retornou mais que o máximo permitido de " . self::MAX_ALLOWED_LIST_RESPONSE . " registros."
+            );
+        }
+
+        return $transactions;
     }
 
     public function create(array $data): Transaction
