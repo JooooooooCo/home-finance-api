b4cadca748e42f79b6faccb8ee0b73bef8c5e45d
Joao Costa <costa.joaomf@gmail.com>
2025-08-26 15:38:33 -0300

    feat: adjust budget result to show transaction categories not defined in budget

--- app/Services/Budget/BudgetResultsService.php
+++ app/Services/Budget/BudgetResultsService.php
@@ -50,15 +50,14 @@ class BudgetResultsService
         }
 
         $budgetCategories = $this->indexCategoriesById($budget['categories']);
-        
+
         $totalRevenue = $this->transactionRepository->getTotalRevenueForPeriod($filters['startDate'], $filters['endDate']);
         $transactions = $this->getExecutedExpenses($filters);
-        $executedBudget = [];        
+        $executedBudget = $this->initializeExecutedBudgetFromCategories($budget['categories'], $totalRevenue);
 
         foreach ($transactions as $transaction) {
-            $executedBudget = $this->calculateResultsPrimaryCategory($executedBudget, $transaction, $budgetCategories, $totalRevenue);
-            $executedBudget = $this->calculateResultsSecondaryCategory($executedBudget, $transaction, $budgetCategories);
-            $executedBudget = $this->calculateResultsSpecificCategory($executedBudget, $transaction, $budgetCategories);
+            $executedBudget = $this->ensureTransactionCategoriesExist($executedBudget, $transaction);
+            $executedBudget = $this->addTransactionToExecutedBudget($executedBudget, $transaction, $budgetCategories);
         }
 
         return $this->removeArrayAssocKeys($executedBudget);
@@ -90,102 +89,6 @@ class BudgetResultsService
         return $plainCategories;
     }
 
-    private function calculateResultsPrimaryCategory(array $executedBudget, array $transaction, array $budgetCategories, float $totalRevenue)
-    {
-        $primaryCategoryId = $transaction['primary_category_id'];
-        $budgetPercentage = isset($budgetCategories[$primaryCategoryId]) ? $budgetCategories[$primaryCategoryId]['budget'] : 0;
-        $budgetAmount =  $budgetPercentage ? round(($budgetPercentage / 100) * $totalRevenue, 2) : 0;
-
-        if (isset($executedBudget[$primaryCategoryId])) {
-            $executedAmount = $executedBudget[$primaryCategoryId]['executed_amount'] ?: 0;
-            $executedAmount += (float) $transaction['amount'];
-            $executedBudget[$primaryCategoryId]['executed_amount'] = round($executedAmount, 2);
-            $executedBudget[$primaryCategoryId]['executed_percentage'] = $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0;
-            return $executedBudget;
-        }
-        
-        $executedAmount = (float) $transaction['amount'];
-
-        $executedBudget[$primaryCategoryId] = [
-            'id' => $primaryCategoryId,
-            'name' => $transaction['primary_category']['name'],
-            'budget_amount' => $budgetAmount,
-            'budget_percentage' => $budgetPercentage,
-            'executed_amount' => round($executedAmount, 2),
-            'executed_percentage' => $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0,
-            'children' => [],
-        ];
-
-        return $executedBudget;
-    }
-
-    private function calculateResultsSecondaryCategory(array $executedBudget, array $transaction, array $budgetCategories)
-    {
-        $primaryCategoryId = $transaction['primary_category_id'];
-        $secondaryCategoryId = $transaction['secondary_category_id'];
-        $baseBudgetAmount = isset($executedBudget[$primaryCategoryId]) ? $executedBudget[$primaryCategoryId]['budget_amount'] : 0;
-        $budgetPercentage = isset($budgetCategories[$primaryCategoryId]['children'][$secondaryCategoryId]) ? 
-            $budgetCategories[$primaryCategoryId]['children'][$secondaryCategoryId]['budget'] : 0;
-        $budgetAmount =  $budgetPercentage ? round(($budgetPercentage / 100) * $baseBudgetAmount, 2) : 0;
-
-        if (isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId])) {
-            $executedAmount = $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'] ?: 0;
-            $executedAmount += (float) $transaction['amount'];
-            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'] = round($executedAmount, 2);
-            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_percentage'] = $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0;
-            return $executedBudget;
-        }
-        
-        $executedAmount = (float) $transaction['amount'];
-
-        $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId] = [
-            'id' => $secondaryCategoryId,
-            'name' => $transaction['secondary_category']['name'],
-            'budget_amount' => $budgetAmount,
-            'budget_percentage' => $budgetPercentage,
-            'executed_amount' => round($executedAmount, 2),
-            'executed_percentage' => $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0,
-            'children' => [],
-        ];
-
-        return $executedBudget;
-    }
-
-    private function calculateResultsSpecificCategory(array $executedBudget, array $transaction, array $budgetCategories)
-    {
-        $primaryCategoryId = $transaction['primary_category_id'];
-        $secondaryCategoryId = $transaction['secondary_category_id'];
-            $specificCategoryId = $transaction['specific_category_id'];
-        $baseBudgetAmount = isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId])
-            ? $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['budget_amount'] : 0;
-        $budgetPercentage = isset($budgetCategories[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId])
-            ? $budgetCategories[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['budget'] : 0;
-        $budgetAmount =  $budgetPercentage ? round(($budgetPercentage / 100) * $baseBudgetAmount, 2) : 0;
-
-        if (isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId])) {
-            $executedAmount = $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'] ?: 0;
-            $executedAmount += (float) $transaction['amount'];
-            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'] = 
-                round($executedAmount, 2);
-            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_percentage'] = 
-                $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0;
-            return $executedBudget;
-        }
-        
-        $executedAmount = (float) $transaction['amount'];
-
-        $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId] = [
-            'id' => $specificCategoryId,
-            'name' => $transaction['specific_category']['name'],
-            'budget_amount' => $budgetAmount,
-            'budget_percentage' => $budgetPercentage,
-            'executed_amount' => round($executedAmount, 2),
-            'executed_percentage' => $budgetAmount > 0 ? round(($executedAmount / $budgetAmount) * 100, 2) : 0,
-            'children' => [],
-        ];
-
-        return $executedBudget;
-    }
 
     private function removeArrayAssocKeys($executedBudget)
     {
@@ -201,4 +104,155 @@ class BudgetResultsService
 
         return $executedBudget;
     }
+
+    private function initializeExecutedBudgetFromCategories(array $categories, float $totalRevenue): array
+    {
+        $executedBudget = [];
+        
+        foreach ($categories as $primaryCategory) {
+            $primaryBudgetAmount = round(($primaryCategory['budget'] / 100) * $totalRevenue, 2);
+            
+            $executedBudget[$primaryCategory['id']] = [
+                'id' => $primaryCategory['id'],
+                'name' => $primaryCategory['name'],
+                'budget_amount' => $primaryBudgetAmount,
+                'budget_percentage' => $primaryCategory['budget'],
+                'executed_amount' => 0,
+                'executed_percentage' => 0,
+                'children' => [],
+            ];
+            
+            if (!empty($primaryCategory['children'])) {
+                foreach ($primaryCategory['children'] as $secondaryCategory) {
+                    $secondaryBudgetAmount = round(($secondaryCategory['budget'] / 100) * $primaryBudgetAmount, 2);
+                    
+                    $executedBudget[$primaryCategory['id']]['children'][$secondaryCategory['id']] = [
+                        'id' => $secondaryCategory['id'],
+                        'name' => $secondaryCategory['name'],
+                        'budget_amount' => $secondaryBudgetAmount,
+                        'budget_percentage' => $secondaryCategory['budget'],
+                        'executed_amount' => 0,
+                        'executed_percentage' => 0,
+                        'children' => [],
+                    ];
+                    
+                    if (!empty($secondaryCategory['children'])) {
+                        foreach ($secondaryCategory['children'] as $specificCategory) {
+                            $specificBudgetAmount = round(($specificCategory['budget'] / 100) * $secondaryBudgetAmount, 2);
+                            
+                            $executedBudget[$primaryCategory['id']]['children'][$secondaryCategory['id']]['children'][$specificCategory['id']] = [
+                                'id' => $specificCategory['id'],
+                                'name' => $specificCategory['name'],
+                                'budget_amount' => $specificBudgetAmount,
+                                'budget_percentage' => $specificCategory['budget'],
+                                'executed_amount' => 0,
+                                'executed_percentage' => 0,
+                                'children' => [],
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+        
+        return $executedBudget;
+    }
+
+    private function ensureTransactionCategoriesExist(array $executedBudget, array $transaction): array
+    {
+        $primaryCategoryId = $transaction['primary_category_id'];
+        $secondaryCategoryId = $transaction['secondary_category_id'];
+        $specificCategoryId = $transaction['specific_category_id'];
+
+        if ($primaryCategoryId && !isset($executedBudget[$primaryCategoryId])) {
+            $primaryName = $transaction['primary_category']['name'] ?? 'Unknown';
+            $executedBudget[$primaryCategoryId] = [
+                'id' => $primaryCategoryId,
+                'name' => $primaryName,
+                'budget_amount' => 0,
+                'budget_percentage' => 0,
+                'executed_amount' => 0,
+                'executed_percentage' => 0,
+                'children' => [],
+            ];
+        }
+
+        if ($secondaryCategoryId && isset($executedBudget[$primaryCategoryId]) &&
+            !isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId])) {
+            $secondaryName = $transaction['secondary_category']['name'] ?? 'Unknown';
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId] = [
+                'id' => $secondaryCategoryId,
+                'name' => $secondaryName,
+                'budget_amount' => 0,
+                'budget_percentage' => 0,
+                'executed_amount' => 0,
+                'executed_percentage' => 0,
+                'children' => [],
+            ];
+        }
+
+        if ($specificCategoryId && isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]) &&
+            !isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId])) {
+            $specificName = $transaction['specific_category']['name'] ?? 'Unknown';
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId] = [
+                'id' => $specificCategoryId,
+                'name' => $specificName,
+                'budget_amount' => 0,
+                'budget_percentage' => 0,
+                'executed_amount' => 0,
+                'executed_percentage' => 0,
+                'children' => [],
+            ];
+        }
+
+        return $executedBudget;
+    }
+
+    private function addTransactionToExecutedBudget(array $executedBudget, array $transaction, array $budgetCategories): array
+    {
+        $primaryCategoryId = $transaction['primary_category_id'];
+        $secondaryCategoryId = $transaction['secondary_category_id'];
+        $specificCategoryId = $transaction['specific_category_id'];
+        $amount = (float) $transaction['amount'];
+
+        if (isset($executedBudget[$primaryCategoryId])) {
+            $executedBudget[$primaryCategoryId]['executed_amount'] += $amount;
+            $executedBudget[$primaryCategoryId]['executed_amount'] = round($executedBudget[$primaryCategoryId]['executed_amount'], 2);
+
+            if ($executedBudget[$primaryCategoryId]['budget_amount'] > 0) {
+                $executedBudget[$primaryCategoryId]['executed_percentage'] = round(
+                    ($executedBudget[$primaryCategoryId]['executed_amount'] / $executedBudget[$primaryCategoryId]['budget_amount']) * 100, 2
+                );
+            }
+        }
+
+        if ($secondaryCategoryId && isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId])) {
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'] += $amount;
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'] =
+                round($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'], 2);
+
+            if ($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['budget_amount'] > 0) {
+                $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_percentage'] = round(
+                    ($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['executed_amount'] /
+                     $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['budget_amount']) * 100, 2
+                );
+            }
+        }
+
+        if ($specificCategoryId && isset($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId])) {
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'] += $amount;
+            $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'] =
+                round($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'], 2);
+
+            if ($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['budget_amount'] > 0) {
+                $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_percentage'] = round(
+                    ($executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['executed_amount'] /
+                     $executedBudget[$primaryCategoryId]['children'][$secondaryCategoryId]['children'][$specificCategoryId]['budget_amount']) * 100, 2
+                );
+            }
+        }
+
+        return $executedBudget;
+    }
+
 }
